b4e2d12ab7c10492ca0f28f956eab37c12aa72fb
github username <github email address>
2020-08-06 18:44:00 -0400

    Installed packages and imported packages into index.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  
   render() {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// set up store in index
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+// any component that we wrap in Prodiver will have access to our redux store, wrap our app in here
+import { Provider } from 'react-redux'
+
 import App from './App';
 
+// set up store here
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
