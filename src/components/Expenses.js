5a85e34b82667f248e600febc78a25cbeecd5a59
github username <github email address>
2020-08-09 17:55:28 -0400

    Passed props down to ExpensesContainer and into expenses, iterated over expenses and returned in div

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Expenses = (props) => {
-
+    console.log(props.expenses)
+    // debugger
     return (
         <div>
-            Inside Expenses Index
+            INside expenses index
+            {/* The first time that props comes thru, the may be undefinied.  You may need to add in a check */}
+           {props.expenses && props.expenses.map(expense =>
+            <li key={expense.id}>{expense.destription} ~ {expense.expense_amount}</li>
+            )}
         </div>
     )
 }
--- src/containers/ExpensesContainer.js
+++ src/containers/ExpensesContainer.js
@@ -8,7 +8,7 @@ class ExpensesContainer extends React.Component {
             <div>Inside Expenses Container
             <ExpenseInput/>
             {/* if this.props.item exists, then find this.props.item.expenses */}
-            <Expenses item={this.props.item && this.props.item.expenses}/>
+            <Expenses expenses={this.props.item && this.props.item.expenses}/>
             </div>
         )
     }
