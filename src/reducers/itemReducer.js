82e96663844ffe9a4e0f43af5e17acc93f4ab3f7
github username <github email address>
2020-08-10 15:13:43 -0400

    In ADD_EXPENSE, finished the return.  All is working

--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -1,5 +1,5 @@
 export default function itemReducer(state = {items: []}, action) {
-    debugger;
+  
     switch (action.type) {
         // you are returning an object with a key of items: that points to an array.  You bring items in from 
         // the initial arguement
@@ -11,14 +11,19 @@ export default function itemReducer(state = {items: []}, action) {
             // of the items along with the new item that is being brought in with action.payload 
             return {...state, items: [...state.items, action.payload]}
        case "ADD_EXPENSE":
-        return {}
-    
-        // this will always return a version of the state, even if it is empty
-   default:
+        let items = state.items.map(item => {
+            if (item.id === action.payload.id) {
+                return action.payload
+            } else {
+                return item
+            }
+        })
+        return {...state, items: items}
+    default:
     return state
     }
 }
-
+ // default will always return a version of the state, even if it is empty
 // responsibe for updating the parts of our store that have to do with the an item
 // reducer = if a function that takes in the previous state as a 1st arguement, then the action object as 
 // a second arguement.  Then inside the reducer, we will object the reducer according to the action type.  
