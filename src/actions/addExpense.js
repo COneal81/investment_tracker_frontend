7aa854c3d057b6350236895c5e00177ed191f8e4
github username <github email address>
2020-08-10 15:12:04 -0400

    In headers, removed Accept: application/json

--- src/actions/addExpense.js
+++ src/actions/addExpense.js
@@ -4,14 +4,13 @@ export const addExpense = (expense, itemId) => {
 
     return (dispatch) => {
         fetch(`http://localhost:3000/api/v1/items/${itemId}/expenses`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept" : "application/json"
-            },
-            body: JSON.stringify(expense)
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(expense)
         })
-       .then(resp => resp.json())
+        .then(response => response.json())
     //    the item that is coming back will have the expense in it because it is nested
        .then(item => dispatch({type: "ADD_EXPENSE", payload: item}))
 
