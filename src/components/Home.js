41a81df96fcfeb17773a00448234f3151b5e90c0
github username <github email address>
2020-08-14 18:25:48 -0400

    Added different logo to NavBar, refactored styling, confirmed all is working! git push

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
-
+import logo from '../images/logo-b.png'
 
 const Home = () => {
 
     return (
-        <div>
+        <div >
             <br></br>
-            <h1>Investment Tracker</h1>
-            <h3><i>...for keeping track of all your investment's expenses!</i></h3>
+            <img src={logo} alt="logo"/>
+            <h3><i>...for keeping track of all your investment expenses!</i></h3>
             <br></br>
             <br></br>
             <br></br>
@@ -16,7 +16,7 @@ const Home = () => {
             <Container>
             <h6>Investment Tracker helps you keep track of the expenses you incur when you purchase 
                 an item to fix up and resell for a profit. It even tells you how much you need to sell 
-                an item at in order to make a profit.</h6>
+                an item for, in order to make a profit!</h6>
             </Container>
         </div>
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {Navbar, Nav} from 'react-bootstrap';
+import navLogo from '../images/smLogo.png'
 
-const NavBar = (props) => {
+const NavBar = () => {
     return(
         
 <Navbar bg="light" expand="sm">
-  <Navbar.Brand as={Link} to="/">Investment Tracker</Navbar.Brand>
+  <Navbar.Brand as={Link} to="/"><img height="50" width="50" src={navLogo} alt="navLogo"/></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
