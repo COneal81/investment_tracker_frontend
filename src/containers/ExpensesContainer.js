fef49ba88a76033a88c06b3b8a039b913f84e288
github username <github email address>
2020-08-28 09:20:16 -0400

    Removed notes used for learning React/Redux

--- src/containers/ExpensesContainer.js
+++ src/containers/ExpensesContainer.js
@@ -6,10 +6,7 @@ class ExpensesContainer extends React.Component {
     render() {
         return (
             <div>
-            {/* you pass down the props from an individual item to be able to add an expense to the existing item and keep the id */}
             <ExpenseInput item={this.props.item}/><br></br>
-            {/* if this.props.item exists, then find this.props.item.expenses */}
-            
             <Expenses expenses={this.props.item && this.props.item.expenses}/>
             </div>
         )
