d04c5faa494555805745f3d2bdc0c19f66aaeecd
github username <github email address>
2020-08-28 09:39:38 -0400

    Removed notes used for learning React/Redux

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,26 +1,13 @@
 import React from 'react'
-import {Route, Link} from 'react-router-dom'
-import Item from './Item'
+import {Link} from 'react-router-dom'
 
 
 import Table from 'react-bootstrap/Table'
 
-// functional b/c it is just presenting a list of items
-// setup a functional component as a regular function.  prefer arrow functions.
-// with a functional component, you get the props from the container and have to 
-// take in the props as an arguement.  Gets the items from the items container, accessing them
-// thru props
-// cannot have a render inside a functional component.  must have a return
-
-// 
 const Items = (props) => {
 
     return (
-        // calling Item in the div passes the child thru props down to the show page to be able to render each 
-            // items attributes on its own page.
         <div>
-            {/* everytime you iterate, make sure to put a key prop in the html element or else you will get a warning  */}
-                
                 <Table responsive striped hover variant="dark" size="sm">
                 
                 <thead>
@@ -43,13 +30,7 @@ const Items = (props) => {
                   </tr>
                  )}
                 </tbody>
-              </Table>
-            
-
-
-           
-            
-            
+              </Table>     
         </div>
     )
 
