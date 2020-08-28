ae02a0b43d196fbebce55c928f8fc89de5de1aec
github username <github email address>
2020-08-28 10:35:57 -0400

    Removed notes used for learning React/Redux

--- src/components/ExpenseInput.js
+++ src/components/ExpenseInput.js
@@ -21,8 +21,6 @@ class ExpenseInput extends React.Component {
 
     handleOnSubmit = (event) => {
         event.preventDefault()
-        // sending this.state that has all of the input info from the form
-        // sending this.props.id so the expense knows which item it belongs to
         this.props.addExpense(this.state, this.props.item.id)
         this.setState({
             destription: "",
@@ -31,12 +29,11 @@ class ExpenseInput extends React.Component {
         })
     }
 
-    // values and names are what makes if a controlled form.
+    
     render() {
         return (
         <div>
             
-
             <Container>
                 <Form onSubmit={this.handleOnSubmit}>
                 <Form.Group controlId= "ExpenseInput.ControlInput2">
@@ -76,19 +73,6 @@ class ExpenseInput extends React.Component {
                 </Form.Group>
                 </Form>
             </Container>
-
-
-            {/* <form onSubmit={this.handleOnSubmit}>
-                <label> Enter an Expense</label><br></br>
-                <input type="text" placeholder="Expense Name" value={this.state.destription} name="destription" onChange = {this.handleOnChange}/> 
-                <label> Amount </label><br></br>
-                <input type="text" placeholder="Amount" value={this.state.expense_amount} name="expense_amount" onChange = {this.handleOnChange}/> 
-                 <label> Date Purchased</label><br></br>
-                 <input type="text" placeholder="Date" value={this.state.date} name="date" onChange = {this.handleOnChange}/> 
-                
-                 <input type="submit"/>
-                
-            </form>  */}
         </div>
         )
     }
