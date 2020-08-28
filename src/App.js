e6ba66b9077c869300b64ca9c09f01bdcce2b650
github username <github email address>
2020-08-28 08:38:28 -0400

    Removed notes used for learning React/Redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import {connect} from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ItemsContainer from './containers/ItemsContainers'
 
-
 class App extends React.Component {
-
- 
 
   render() {
     return (
       <div className="App">
-      
         <header className="App-header">
-         
          <ItemsContainer/>
         </header>
       </div>
@@ -24,24 +18,5 @@ class App extends React.Component {
     
 }
 
-// mapStateToProps is a way of accesses our current values in our store as props, we have to pass it to 
-// connect, b/c connect is our way of connecting our redux store to a specific component
-// const mapStateToProps = (state) => {
-//   return {
-//     items: state.items
-//   }
-// }
 
 export default App;
-// mapDIspatchToProps - gives us the abiity to update our store from this component
-//  - can also be passed as an action creator like {fetchItems}
-
-// connect automatically calls dispatch for us on the return value of fetchItems. It returns an 
-// action object that we have passed in. ex. store.dispatch({type: 'FETCH_ITEMS', payload: {item_name: "Desk"})
-
-// - connect sends action object to action file(ex. fetchItems) which returns action to connect which is 
-// dispatched to our store which is send into our reducer as the action (This is dispatch happening
-// behind the scenes acutmatically with connect)
-// 
-// 
-// A App can also be a container depending on how you set it up.
