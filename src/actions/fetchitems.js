e548461dab3fccc489b5f3f57ace284efa6d9f57
github username <github email address>
2020-09-02 14:24:34 -0400

    Changed fetch url to localhost from ip address

--- src/actions/fetchitems.js
+++ src/actions/fetchitems.js
@@ -2,7 +2,7 @@
 
 export function fetchItems(action) {
     return (dispatch) => {
-        fetch('http://127.0.0.1:3000/api/v1/items')
+        fetch('http://localhost:3000/api/v1/items')
     .then(resp => resp.json())
     .then(items => dispatch({
         type:"FETCH_ITEMS",
